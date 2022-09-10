29be77319e82bd20085c78a1817ad00bdf809ddf
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-09-10 13:46:37 +0300

    Problem 5 added

---
+++ 01.Recursion_and_Backtracking/02.Excercise/04.connected_ares_in_matrix.py
@@ -0,0 +1,43 @@
+class Area:
+    def __init__(self, row, col, size):
+        self.row = row
+        self.col = col
+        self.size = size
+
+
+def explore_area(row, col, matrix):
+    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
+        return 0
+    if matrix[row][col] != "-":
+        return 0
+
+    matrix[row][col] = "v"
+    area_size = 1
+    area_size += explore_area(row - 1, col, matrix)
+    area_size += explore_area(row + 1, col, matrix)
+    area_size += explore_area(row, col - 1, matrix)
+    area_size += explore_area(row, col + 1, matrix)
+
+    return area_size
+
+
+rows = int(input())
+columns = int(input())
+
+matrix = []
+for _ in range(rows):
+    matrix.append(list(input()))
+
+areas = []
+
+for row in range(rows):
+    for col in range(columns):
+        size = explore_area(row, col, matrix)
+        if size == 0:
+            continue
+        areas.append(Area(row, col, size))
+
+print(f'Total areas found: {len(areas)}')
+for index, area in enumerate(sorted(areas, key=lambda a: a.size, reverse=True)):
+    print(f'Area #{index + 1} at ({area.row}, {area.col}), size: {area.size}')
+
