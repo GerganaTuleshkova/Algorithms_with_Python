0ed95063ba4f77f0754ad49ff925878edc1f8a7c
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-09-11 14:17:23 +0300

    Problem 4 updated solution - optimized

--- 02.Searching_and_Sorting/01.Lab/04.insertion_sort.py
+++ 02.Searching_and_Sorting/01.Lab/04.insertion_sort.py
@@ -4,5 +4,7 @@ for i in range(1, len(numbers)):
     for j in range(i, 0, -1):
         if numbers[j] < numbers[j - 1]:
             numbers[j], numbers[j - 1] = numbers[j - 1], numbers[j]
+        else:
+            break
 
 print(*numbers, sep=' ')
